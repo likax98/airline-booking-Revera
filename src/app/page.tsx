f475a3e5a34490250e857df4e35bbc247e76ef1e
lika <lika@MacBook-Pro.local>
2025-08-06 17:30:21 +0400

    fix(home): wrap with Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
+
 import { destinations } from "@/data";
 import { AirlineForm } from "@/components/home/AirlineForm";
 
-
-const Home = () => <AirlineForm {...{ destinations }} />;
+const Home = () => (
+  <Suspense fallback={<div>Loading form...</div>}>
+    <AirlineForm {...{ destinations }} />
+  </Suspense>
+);
 
 export default Home;
