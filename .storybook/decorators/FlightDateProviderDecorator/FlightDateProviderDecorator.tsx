00952f3953c5e0bf4578f773f7c442d163ed3e37
lika <lika@MacBook-Pro.local>
2025-08-06 16:52:15 +0400

    chore(package): update test script

--- .storybook/decorators/FlightDateProviderDecorator/FlightDateProviderDecorator.tsx
+++ .storybook/decorators/FlightDateProviderDecorator/FlightDateProviderDecorator.tsx
@@ -1,5 +1,4 @@
 import type { Decorator } from "@storybook/react";
-
 import { FlightDateFieldProvider } from "@/components/home/AirlineForm/context";
 
 export const withMockFlightDateFieldContext: Decorator = (
